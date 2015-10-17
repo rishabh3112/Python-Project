b92e534c383ff42c4f066d8d139834a915ac4a1e
Arsh23 <arsh.bbps@gmail.com>
2015-10-17 14:56:32 +0530

    added file

---
+++ code.py
@@ -0,0 +1,41 @@
+# basic class for any object
+class object():
+
+	x=0
+	y=0
+	h=0 
+	w=0
+
+	img = 'abcd.png'
+	can_collide = True
+
+	def draw(x,y):
+
+
+	def collide(py,py):
+
+
+	def any_special_func():
+
+
+class wall():
+
+	x=0
+	y=0
+	h=0
+	w=0
+
+	img = 'abcd.png'
+	can_collide = True
+
+	def draw(x,y):
+
+
+	def collide(py,py):
+
+
+	def any_special_func():
+
+
+
+
