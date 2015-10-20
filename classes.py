204cfd18b875ed11a788b2ebea6fe1ec9a6d8d2f
Arsh23 <arsh.bbps@gmail.com>
2015-10-20 19:36:16 +0530

    Divided code into two files and made minor changes

---
+++ classes.py
@@ -0,0 +1,23 @@
+class player():
+
+    x = 0.0
+    y = 0.0
+    h = 0.0
+    w = 0.0
+    speed = 0.0
+    health = 100.0
+    armour = 0.0
+
+    powerup1 = False
+    powerup2 = False
+    powerup3 = False
+
+    def move():
+        pass
+
+    def shoot():
+        pass
+
+    def interact():
+        pass
+    
