5e3da87eaa0525cbe072ec738b474b3ccd12aba5
Arsh23 <arsh.bbps@gmail.com>
2015-10-20 21:46:10 +0530

    Added player movement code

--- classes.py
+++ classes.py
@@ -1,19 +1,58 @@
+import pygame
+pygame.init()
+
+brakespeed = 0.05
+
 class player():
 
-    x = 0.0
-    y = 0.0
-    h = 0.0
-    w = 0.0
-    speed = 0.0
-    health = 100.0
-    armour = 0.0
+    def __init__(self):
+        #initialize all variables
 
-    powerup1 = False
-    powerup2 = False
-    powerup3 = False
+        self.x = 0.0
+        self.y = 0.0
+        self.h = 20
+        self.w = 20
+        self.speedx = 0.0
+        self.speedy = 0.0
+        self.health = 100.0
+        self.armour = 0.0
 
-    def move():
-        pass
+        self.powerup1 = False
+        self.powerup2 = False
+        self.powerup3 = False
+
+        self.image = pygame.image.load('player.png')
+        self.image = pygame.transform.scale(self.image, (self.w, self.h))
+
+    def move(self,direction):
+        #set initial speed according to direction
+
+        if direction == 'up':
+            self.speedy = -5
+        elif direction == 'down':
+            self.speedy = 5
+        elif direction == 'left':
+            self.speedx = -5
+        elif direction == 'right':
+            self.speedx = 5      
+
+    def inertia(self):
+        #constantly reduce speed till it is zero for smooth motion
+        #brakespeed is low to see the slowing effect
+
+        if self.speedx > 0:
+            self.speedx = (self.speedx*1000 - brakespeed*1000)/1000
+        elif self.speedx < 0:
+            self.speedx = (self.speedx*1000 + brakespeed*1000)/1000
+
+        if self.speedy > 0:
+            self.speedy = (self.speedy*1000 - brakespeed*1000)/1000
+        elif self.speedy < 0:
+            self.speedy = (self.speedy*1000 + brakespeed*1000)/1000
+
+        #always adds speed to x and y coords
+        self.x += self.speedx
+        self.y += self.speedy
 
     def shoot():
         pass
--- code.py
+++ code.py
@@ -1,76 +1,93 @@
-# basic class for any object
-'''
-class objects():
+import pygame
+from classes import *
 
-	x=0
-	y=0
-	h=0 
-	w=0
+pygame.init()
 
-	img = 'abcd.png'
-	can_collide = True
+#global variables
+display_width = 800
+display_height = 600
+player1 = player()
 
-	def draw(x,y):
+#color variables
+black = (0,0,0)
+white = (255,255,255)
 
+# main pygame variables
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+clock = pygame.time.Clock()
 
-	def collide(py,py):
-            
-
-
-	def any_special_func():
-
-'''
-class player():
-
-	x=0
-	y=0
-	h=0
-	w=0
-
-	img = 'player.png'
-    powerup = False
-
-    health = 100
-    armour = 0
-    speed = 0
-
-	def move(btn_pressed):
-
-#	def shoot(mx,my):
-
-#	def interact(obj):
-
-#	def message(str):
-
-#	def special():
-
-#	def check_powerup():
-'''
 
 def menu():
-
+    pass
 
 def gameinit():
-'''
+    #initialise player
+    player1.x = 0.0
+    player1.y = 0.0
+    player1.health = 100
+    player1.speed = 0.0
+    #etc
+            
 
 def gameloop():
+   
+    quit = False
+    while quit == False:
 
-	#events
+   	    #events
+        for event in pygame.event.get():
+        
+            # event - quit
+            if event.type == pygame.QUIT:
+                quit = True
+
+            #this event runs when any key is pressed
+            if event.type == pygame.KEYDOWN:
+
+                #these if statements check for which key is being pressed
+                if event.key == pygame.K_LEFT:
+                    player1.move('left')
+                elif event.key == pygame.K_RIGHT:
+                    player1.move('right')
+                elif event.key == pygame.K_UP:
+                    player1.move('up')
+                elif event.key == pygame.K_DOWN:
+                    player1.move('down')
+ 
+
+            #used for debugging
+            print event
+
+
+        #logic
+        player1.inertia()
+        
+
+	    #map draw
+        gameDisplay.fill(white)
+        draw(player1)
+        pygame.display.update()
+        clock.tick(60)
 
 
 
-	#logic
 
 
-
-	#map draw
-
-
-'''
 def gameexit():
+    #runs before closing the window
+    pygame.quit()
+    quit()
 
 
+def draw(obj):
+    #used to draw any object sent to it on the screen
+    gameDisplay.blit(obj.image, (obj.x,obj  .y))
 
+
+# main code
+gameinit()
+gameloop()
+gameexit()
 
 
 
